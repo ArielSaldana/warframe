a25719caa39736f5c43e1d7047c2852010370676
Ariel Saldana <ariel@unreal.io>
2023-06-01 17:13:48 -0400

    WIP

---
+++ include/event-emitter/eventmessage.h
@@ -0,0 +1,21 @@
+//
+// Created by ariel on 6/1/23.
+//
+
+#ifndef WARFRAME_EVENTMESSAGE_H
+#define WARFRAME_EVENTMESSAGE_H
+
+#include "gatewayevents/hello_gateway_event.h"
+#include "payload.h"
+
+template<typename T>
+struct EventMessage {
+    std::string event_name;
+    Payload<T> payload;
+    EventMessage(const std::string &_event_name, Payload<T> &_payload)
+        : event_name(_event_name), payload(_payload) {
+    }
+};
+
+
+#endif//WARFRAME_EVENTMESSAGE_H
